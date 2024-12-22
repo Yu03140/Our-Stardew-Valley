229a68ba7a381591a39fcb3d02b4c9d5689fcd77
16pomegranates <18904068639@163.com>
2024-12-23 03:10:01 +0800

    Add files via upload

---
+++ TaskBarLayer.h
@@ -0,0 +1,18 @@
+﻿#ifndef TASKBARLAYER_H
+#define TASKBARLAYER_H
+
+#include "cocos2d.h"
+#include "TaskBar.h"
+
+class TaskBarLayer : public cocos2d::Layer {
+public:
+    virtual bool init();
+    CREATE_FUNC(TaskBarLayer);
+    static cocos2d::Scene* createScene();
+
+private:
+    TaskBar* taskBar;
+
+};
+
+#endif // TASKBARLAYER_H
