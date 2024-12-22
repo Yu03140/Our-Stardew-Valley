48c7a8423d9292be85755c7dcf6ad9cd1b473a13
61333333333333 <728617303@qq.com>
2024-12-22 16:36:36 +0800

    resources

--- cocos/Resources/newnew/paths.tsx
+++ cocos/Resources/newnew/paths.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="paths" tilewidth="16" tileheight="16" tilecount="64" columns="4">
- <image source="paths.png" width="64" height="256"/>
+<tileset version="1.10" tiledversion="1.11.0" name="paths" tilewidth="16" tileheight="16" tilecount="64" columns="4">
+ <image source="Content/Maps/paths.png" width="64" height="256"/>
 </tileset>
