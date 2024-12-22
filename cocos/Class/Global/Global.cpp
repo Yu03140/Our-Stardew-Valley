3882545433aa037b3e986ac0b7ac4f74e53a4a3d
BaHoon <728617303@qq.com>
2024-12-23 05:09:44 +0800

    Create Global.cpp

---
+++ cocos/Class/Global/Global.cpp
@@ -0,0 +1,15 @@
+#include "Global.h"
+
+TimeSystem* timeSystem = nullptr;
+BackpackLayer* backpackLayer = nullptr;
+
+cocos2d::Vec2 character_pos = cocos2d::Vec2::ZERO;
+cocos2d::Vec2 MOUSE_POS = cocos2d::Vec2::ZERO;
+bool is_infarm;
+bool is_in_control = 0;
+
+float SceneWidth;
+float SceneHeight;
+
+
+
